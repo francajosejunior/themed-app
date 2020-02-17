ae2743eec540aa059a2347b037129f4234a65de5
José Joaquim Júnior <franca.josejunior@outlook.com>
2020-02-17 17:49:05 -0300

    productivity-simulator-wrapper

--- src/features/dashboard/index.js
+++ src/features/dashboard/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import UserLevel from '../userLevel';
+import './styles.scss';
 
 const Dashboard = () => {
   return (
     <div className="dashboard">
       <UserLevel />
+      <div className="productivity-simulator-wrapper">
+        <div className="productivity"></div>
+        <div className="simulator"></div>
+      </div>
     </div>
   );
 };
