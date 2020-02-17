412a466c663bb67d9d9c4eba81ea89cc3d9209cc
José Joaquim Júnior <franca.josejunior@outlook.com>
2020-02-17 18:36:28 -0300

    Alert

--- src/features/productivity/index.js
+++ src/features/productivity/index.js
@@ -5,8 +5,12 @@ import Alert from '../../components/alert';
 
 const Productivity = () => {
   return (
-    <div>
-      <Alert icon={<MaterialIcon icon="eye" size="small" inactive={true} />}>
+    <div className="font-color-label">
+      <Alert
+        icon={
+          <MaterialIcon icon="remove_red_eye" size="small" inactive={true} />
+        }
+      >
         Produtividade
       </Alert>
     </div>
